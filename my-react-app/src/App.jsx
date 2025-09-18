82d0cc3e4b41b98fc22bbf39ec6d6f0f61896f72
butterfly0204 <favourmogaji3@gmail.com>
2025-09-18 12:21:07 -0500

    begining of chaos

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,13 +1,20 @@
-import Button from './Button.jsx'
 
+import Students from "./Students.jsx";
 
 function App(){
-  return(  
-   <Button></Button> 
-    
-)
+  return(
+    <> 
+    <Students name=" Mogaji Favour" age= {20} isStudent={false}></Students>
+    <Students name=" oluwabukola" age= {30} isStudent={true}></Students>
+    <Students name=" peace" age= {15} isStudent={false}></Students>
+    <Students name="chisom" age ={23} isStudent={true}/>
   
- 
+    </>
+  )
 }
+
+
+
+
 
 export default App
---
+++ my-react-app/src/Students.jsx
@@ -0,0 +1,10 @@
+
+import PropTypes from "prop-types";
+
+function Students(props){
+   
+}
+
+
+
+export default Students
