369c81e677ee0c4d489d32f7269bfbb45f146e5b
butterfly0204 <favourmogaji3@gmail.com>
2025-09-26 19:34:06 -0500

    it is well

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,16 +1,19 @@
+import Header from './Header.jsx'
+import Footer from './Footer.jsx';
+import Food from './Food.jsx';
 
-import UserGreeting from "./UserGreeting.jsx";
-function App(){
-    return(
-      <>
-      <UserGreeting isLoggedIn ={false} username= "favour"> </UserGreeting>
-      </>
+function App() {
   
-  )
+  return(
+    <>
+      <Header/>
+      <Food/>
+      <Footer/>
+    
+      </>
+  );
+
+
 }
-
-
-
-
 
 export default App
