468a65e785868e1e48ef7dcd21868564f255dfa5
butterfly0204 <favourmogaji3@gmail.com>
2025-10-02 19:15:26 -0500

    okay

--- my-react-app/src/List.jsx
+++ my-react-app/src/List.jsx
@@ -1,8 +1,12 @@
 function List(){
 
-        const fruit = ["apple", "Mango", "orange", "strawBeries", "grapes"];
+        const fruits = [ {name:"apple", calories: 96},
+                         {name:"Mango", calories: 30}, 
+                         {name: "orange", calories:50}, 
+                         {name:"strawBeries", calories: 20},
+                         {name: "grapes", calories: 62}];
 
-        const listItems =fruit.map(fruit => <li>{fruit}</li>)
+        const listItems =fruits.map(fruit => <li>{fruit}</li>)
 
         return ( <ul>{listItems}</ul>)
         
