a97235f793a4c0ddf7a31985a220d2e8befe5446
butterfly0204 <favourmogaji3@gmail.com>
2025-10-02 21:29:36 -0500

    omo

--- my-react-app/src/List.jsx
+++ my-react-app/src/List.jsx
@@ -1,14 +1,16 @@
 function List(){
 
-        const fruits = [ {name:"apple", calories: 96},
-                         {name:"Mango", calories: 30}, 
-                         {name: "orange", calories:50}, 
-                         {name:"strawBeries", calories: 20},
-                         {name: "grapes", calories: 62}];
+        const fruits = [ {id: 1, name:"Apple", calories: 96},
+                         {id: 2, name:"Mango", calories: 30}, 
+                         {id:3, name: "Orange", calories:50}, 
+                         {id:4,name:"strawBeries", calories: 20},
+                         {id:5,name: "Grapes", calories: 62}];
 
-        const listItems =fruits.map(fruit => <li>{fruit}</li>)
+        const listItems =fruits.map(fruit => <li key={fruit.id}>
+                                                        {fruit.name}:&nbsp; 
+                                                       <b>{fruit.calories}</b></li>)
 
-        return ( <ul>{listItems}</ul>)
+        return ( <ol>{listItems}</ol>)
         
 
 }
